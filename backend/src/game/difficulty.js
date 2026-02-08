a7a651a39592597d6e18fe6ea7d9cf3db23ead9b
AtCloudDiwash <diwash.kuskusmiya@gmail.com>
2026-02-08 14:51:59 -0600

    adjust difficult; added game names

--- backend/src/game/difficulty.js
+++ backend/src/game/difficulty.js
@@ -2,11 +2,11 @@ export const difficulties = {
   easy: {
     answer_time: 60, // seconds
     discussion_time: 60, // seconds
-    voting_time: 30
+    voting_time: 15
   },
   hard: {
     answer_time: 60, // seconds
     discussion_time: 0, // seconds
-    voting_time: 30
+    voting_time: 15
   },
 };
